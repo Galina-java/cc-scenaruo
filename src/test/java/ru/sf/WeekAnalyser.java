73a8cddf0dc6b61360691910e18acf7fd3dfdb1b
Galina <galya_1989@mail.ru>
2023-02-08 12:00:17 +0300

    35 modul

---
+++ src/test/java/ru/sf/WeekAnalyser.java
@@ -0,0 +1,12 @@
+package ru.sf;
+
+public class WeekAnalyser {
+        public static String isItFridayToday(String today) {
+            if (today.equals("Friday")) {
+                return "Yes";
+            } else {
+                return "Nope";
+            }
+
+        }
+}
